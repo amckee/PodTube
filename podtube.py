892e05162210c201a4d3ec18cb98e21bd0d38fda
Adam <armware+git@gmail.com>
2023-04-23 01:11:39 -0500

    Automatic version updating via git pre-commit hook

--- podtube.py
+++ podtube.py
@@ -10,7 +10,7 @@ import misaka
 import youtube, bitchute, rumble, dailymotion
 from tornado import ioloop, web
 
-# BE SURE TO UPDATE THIS - ci/cd depends on it
+# Handled by git pre-commit hook
 __version__ = 'v2023.04.22.1'
 
 class FileHandler(web.RequestHandler):
