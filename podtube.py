7205e5e3f0c57df0493aaccb714e41287746997f
Adam McKee <armware@gmail.com>
2024-02-05 19:18:55 -0600

    Add ability to limit number of pages pulled from

--- podtube.py
+++ podtube.py
@@ -11,7 +11,7 @@ import youtube, bitchute, rumble, dailymotion
 from tornado import ioloop, web
 
 # Handled automatically by git pre-commit hook
-__version__ = '2024.02.02.2'
+__version__ = '2024.02.05.1'
 
 class FileHandler(web.RequestHandler):
     def get(self):
