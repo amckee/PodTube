193192573e24e75fefcc4a5e1955c162ae2c2e8a
Adam <armware+git@gmail.com>
2024-02-07 19:57:10 -0600

    Update version

--- podtube.py
+++ podtube.py
@@ -11,7 +11,7 @@ import youtube, bitchute, rumble, dailymotion
 from tornado import ioloop, web
 
 # Handled automatically by git pre-commit hook
-__version__ = '2024.02.07.1'
+__version__ = '2024.02.07.2'
 
 class FileHandler(web.RequestHandler):
     def get(self):
