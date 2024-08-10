297f9f4a977ace03c5d743138ec2f3abf7fc894d
Adam <armware+git@gmail.com>
2024-08-09 21:37:51 -0500

    Attempting to fix Youtube

--- podtube.py
+++ podtube.py
@@ -16,7 +16,7 @@ import rumble
 import dailymotion
 
 # Handled automatically by git pre-commit hook
-__version__ = '2024.07.09.4'
+__version__ = '2024.08.08.1'
 
 class FileHandler(web.RequestHandler):
     def get(self):
