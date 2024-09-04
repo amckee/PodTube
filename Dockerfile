d7c71abab89ce6cd9647c3de95049f2b961098d5
Adam McKee <armware@gmail.com>
2024-09-04 13:07:57 -0500

    Need to use sudo for these steps

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM python:3.10
 
-LABEL net.ftawesome.home.version='2024.08.19.1'
+LABEL net.ftawesome.home.version='2024.09.04.1'
 
 WORKDIR /opt/
 
--- podtube.py
+++ podtube.py
@@ -16,7 +16,7 @@ import rumble
 import dailymotion
 
 # Handled automatically by git pre-commit hook
-__version__ = '2024.08.30.1'
+__version__ = '2024.09.04.1'
 
 class FileHandler(web.RequestHandler):
     def get(self):
