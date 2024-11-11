e9c79982a6c3330137c4eef4f0ee01ba01649245
Adam McKee <armware+git@gmail.com>
2024-11-11 15:16:59 -0600

    Add requirement

--- podtube.py
+++ podtube.py
@@ -16,7 +16,7 @@ import rumble
 import dailymotion
 
 # Handled automatically by git pre-commit hook
-__version__ = '2024.11.11.2'
+__version__ = '2024.11.11.3'
 
 class FileHandler(web.RequestHandler):
     def get(self):
