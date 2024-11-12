62b4a6b7502b3497bbb28561c8cc1eaf80b5d41d
Adam McKee <armware+git@gmail.com>
2024-11-11 19:44:06 -0600

    Fix requirements

--- youtube.py
+++ youtube.py
@@ -19,15 +19,15 @@ from tornado import gen, httputil, ioloop, iostream, process, web
 from tornado.locks import Semaphore
 import utils
 
-import ssl
-from pytube.innertube import _default_clients
+# import ssl
+# from pytube.innertube import _default_clients
 
-_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
-_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
+# _default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+# _default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+# _default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+# _default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+# _default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+# _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
 
 key = None
 cleanup_period = None
@@ -47,38 +47,38 @@ __version__ = 'v2024.11.11.1'
 conversion_queue = {}
 converting_lock = Semaphore(2)
 
-import pytube, re
-def patched_get_throttling_function_name(js: str) -> str:
-    function_patterns = [
-        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
-        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
-    ]
-    for pattern in function_patterns:
-        regex = re.compile(pattern)
-        function_match = regex.search(js)
-        if function_match:
-            if len(function_match.groups()) == 1:
-                return function_match.group(1)
-            idx = function_match.group(2)
-            if idx:
-                idx = idx.strip("[]")
-                array = re.search(
-                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
-                        nfunc=re.escape(function_match.group(1))),
-                    js
-                )
-                if array:
-                    array = array.group(1).strip("[]").split(",")
-                    array = [x.strip() for x in array]
-                    return array[int(idx)]
+# import pytube, re
+# def patched_get_throttling_function_name(js: str) -> str:
+#     function_patterns = [
+#         r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+#         r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+#         r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+#     ]
+#     for pattern in function_patterns:
+#         regex = re.compile(pattern)
+#         function_match = regex.search(js)
+#         if function_match:
+#             if len(function_match.groups()) == 1:
+#                 return function_match.group(1)
+#             idx = function_match.group(2)
+#             if idx:
+#                 idx = idx.strip("[]")
+#                 array = re.search(
+#                     r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+#                         nfunc=re.escape(function_match.group(1))),
+#                     js
+#                 )
+#                 if array:
+#                     array = array.group(1).strip("[]").split(",")
+#                     array = [x.strip() for x in array]
+#                     return array[int(idx)]
 
-    raise RegexMatchError(
-        caller="get_throttling_function_name", pattern="multiple"
-    )
+#     raise RegexMatchError(
+#         caller="get_throttling_function_name", pattern="multiple"
+#     )
 
 # ssl._create_default_https_context = ssl._create_unverified_context
-pytube.cipher.get_throttling_function_name = patched_get_throttling_function_name
+# pytube.cipher.get_throttling_function_name = patched_get_throttling_function_name
 
 def get_env_or_config_option(conf: ConfigParser, env_name: str, config_name: str, default_value = None):
     """
