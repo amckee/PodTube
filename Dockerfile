4eda7437fed223d21fe835956cf3c0c15c82e190
Adam McKee <armware+fixmegitconf@gmail.com>
2026-05-01 15:37:52 -0500

    Try to simplify workflows, more version specifics put back

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM python:3.12
 
-LABEL net.ftawesome.home.version='2026.05.01.4'
+LABEL net.ftawesome.home.version='2026.05.01.5'
 
 WORKDIR /opt/
 
--- podtube.py
+++ podtube.py
@@ -18,7 +18,7 @@ import rumble
 import dailymotion
 
 # Handled automatically by git pre-commit hook
-__version__ = '2026.05.01.4'
+__version__ = '2026.05.01.5'
 
 class FileHandler(web.RequestHandler):
     """Handles requests for a specific file."""
