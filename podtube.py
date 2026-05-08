146ae9c631b215c57ef6ab0b6d7758063ceeb933
Adam McKee <armware+git@gmail.com>
2026-05-08 16:52:55 -0500

    Use fixed branch

--- podtube.py
+++ podtube.py
@@ -18,7 +18,7 @@ import rumble
 import dailymotion
 
 # Handled automatically by git pre-commit hook
-__version__ = '2026.05.01.12'
+__version__ = '2026.05.07.1'
 
 class FileHandler(web.RequestHandler):
     """Handles requests for a specific file."""
